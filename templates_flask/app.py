571f03ddb935e600cc053d49e56efff2de3d66d8
camillaoggioni <camilla1.oggioni@iisgalvanimi.edu.it>
2023-05-05 09:43:37 +0000

    5 maggio

---
+++ templates_flask/app.py
@@ -0,0 +1,21 @@
+from flask import render_template
+from flask import Flask
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database= "MARVEL"
+)
+mycursor = mydb.cursor()
+
+
+app = Flask(__name__)
+
+
+@app.route('/personaggi')
+def personaggList():
+    mycursor.execute("SELECT * FROM Marvel_Characters")
+    myresult= mycursor.fetchall()
+    return render_template('marvel_personaggi.html', personaggi=myresult)
